9cdb0b401d0d4167a0c836e6f66c18a37e2a95e7
Rory Dent <whishana@gmail.com>
2016-06-10 00:02:30 +0200

    Things almost in place
    - added mocha, working tests in place
    - React simple element works
    - object rest and spread works

---
+++ test/tests.js
@@ -0,0 +1,76 @@
+const expect = require('expect')
+const deepFreeze = require('deep-freeze')
+
+import { todos } from '../reducers/reducers'
+
+describe('todos reducer', function () {
+  it('should testAddTodo', function () {
+    testAddTodo()
+  })
+  it('should testToggleTodo', function () {
+    testToggleTodo()
+  })
+})
+
+const testAddTodo = () => {
+  const stateBefore = []
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'learn Redux'
+  }
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'learn Redux',
+      completed: false
+    }
+  ]
+
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter)
+}
+
+ const testToggleTodo = () => {
+  let stateBefore = [
+    {
+      id: 0,
+      text: 'yo',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'dude',
+      completed: false
+    }
+  ]
+
+  let action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  }
+
+  let stateAfter = [
+    {
+      id: 0,
+      text: 'yo',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'dude',
+      completed: true
+    }
+  ]
+
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  entry: './index.js',
+  entry: './main.js',
   output: {
     path: './',
     filename: 'bundle.js'
   },
-  loaders: [
-    {
-      test: /\.(jsx|js)/,
-      loaders: [
-        'babel'
-      ],
-      include: 'index.js',
-      query: {
-        presets: [
-          'es2015'
-        ]
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread']
+        }
       }
-    }
-  ],
+    ]
+  },
   devServer: {
-    contentBase: ''
+    inline: true,
+    port: 3333
   },
   watch: true
 }
